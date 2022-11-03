dbca99a564644391b434bf9c22575731b80af016
JulioCabrera203446 <203446@ids.upchiapas.edu.mx>
2022-11-03 11:20:25 -0600

    corte 2 examen

--- Corte2/Exam.py
+++ Corte2/Exam.py
@@ -1,39 +1,20 @@
-#Julio Antonio Cabrera Yañez - 203446
+# Gerson Efher Lopez
+# Julio Antonio Cabrera Yañez
 
-import threading
-import random
-import queue
+import threading, random
 import time
+from queue import Queue
 
-capacidad = queue.Queue(maxsize=20)
-meseros = queue.Queue(maxsize=2)
-cocineros = queue.Queue(maxsize=2)
-clientes_pause = queue.Queue()
-reservaciones = queue.Queue(maxsize=4) 
-reservaciones_pause = queue.Queue()
-ordenes = queue.Queue()
-ordenes_finalizadas = queue.Queue()
+mutex = threading.Lock()
 
-lock = threading.Lock()
+MESEROS = 2
+COCINEROS = 2
+clientes = Queue(maxsize=20)
+cola_clientes = Queue()
+reservaciones = Queue(maxsize=4) 
+ordenes = Queue()
+ordenes_listas = Queue()
 
-class Cliente(threading.Thread):
-    def __init__(self, id):
-        threading.Thread.__init__(self)
-        self.monitor = threading.Condition()
-
-    def comer(self):
-        while True:
-            with self.monitor:
-                if not ordenes_finalizadas.empty():
-                    id_cliente = capacidad.get()
-                    print(f'Cliente {id_cliente} esta comiendo su platillo')
-                    time.sleep(random.randint(4, 7))
-                    print(f'Cliente {id_cliente} termina de comer y se retira')
-                    ordenes.get()
-                    self.monitor.notify()
-
-    def run(self):
-        self.comer()
 
 class Reservacion(threading.Thread):
     def __init__(self, id):
@@ -44,46 +25,75 @@ class Reservacion(threading.Thread):
     def reservar(self):
         while True:
             with self.monitor:
-                if not reservaciones.full():
+                if reservaciones.full() == False:
                     reservaciones.put(self.id)
-                    print(f'Cliente {self.id} de reservaciones reserva un lugar')
-                    self.monitor.notify()
-                    time.sleep(15)
+                    print("Cliente " +str(self.id)+ " ha reservado un lugar")
+                    time.sleep(40)
+                    self.id += 1
                 else:
-                    print(f'Cliente {self.id} intento reservar pero no tuvo exito, ya no hay cupo, por lo tanto, se va a la cola de espera')
-                    clientes_pause.put(self.id)
-                    time.sleep(25)
-                self.id = self.id + 1
+                    print("Cliente " +str(self.id)+ " no pudo reservar, se mando a la cola de clientes")
+                    cola_clientes.put(self.id)
+                    time.sleep(40)
+                    self.id += 1
 
     def run(self):
         self.reservar()
 
-class Mesero(threading.Thread):
+
+class Atender(threading.Thread):
+    def __init__(self, id):
+        threading.Thread.__init__(self)
+        self.id = id
+        self.monitor = threading.Condition()
+
+    def atender_cliente(self):
+        while True:
+            with self.monitor:
+                if cola_clientes.empty():
+                    if reservaciones.empty() == False:
+                        reservaciones.get()
+                        print("Cliente " +str(self.id)+ " con reservacion se atendio")
+                        clientes.put(self.id)
+                        time.sleep(3)
+                    else:
+                        if clientes.full() == False:
+                            clientes.put(self.id)
+                            print("Cliente " +str(self.id)+ " ha sido atendido")
+                            time.sleep(3)
+                        else:
+                            print("Cliente " +str(self.id)+ " no pudo ser atendido, se mando a la cola de espera")
+                            cola_clientes.put(self.id)
+                            time.sleep(4)
+                else:
+                    print("Cliente " +str(self.id)+ " no pudo ser atendido, se mando a la cola de espera")
+                    cola_clientes.put(self.id)
+                    time.sleep(4)
+                self.id += 1
+
+    def run(self):
+        self.atender_cliente()
+
+
+class Cliente(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
         self.monitor = threading.Condition()
 
-    def atender(self):
+    def comer(self):
         while True:
             with self.monitor:
-                if not capacidad.empty() and capacidad.qsize()>1:
-                    if meseros.empty():
-                        meseros.put(1)
-                        print(f'Un mesero esta disponible y tomando la orden')
-                        time.sleep(3)
-                        print(f'La orden esta en proceso')
-                        ordenes.put(1)
-                        meseros.get()
-                        self.monitor.notify()
-                    else:
-                        print(f'Los meseros no se encuentran disponibles')
-                        time.sleep(3)
-                else:
-                    print(f'Los meseros estan descansando')
-                    time.sleep(3)
+                if ordenes_listas.empty() == False:
+                    client_id = clientes.get()
+                    print("Cliente " +str(client_id)+ " esta comiendo...")
+                    ordenes_listas.get()
+                    time.sleep(2)
+                    print("Cliente " +str(client_id)+ " termino de comer y se fue del restaurante")
+                    time.sleep(4)
+            time.sleep(1)
 
     def run(self):
-        self.atender()
+        self.comer()
+
 
 class Cocinero(threading.Thread):
     def __init__(self):
@@ -92,69 +102,60 @@ class Cocinero(threading.Thread):
 
     def cocinar(self):
         while True:
-            with self.monitor:
-                if not ordenes.empty():
-                    if cocineros.empty():
-                        cocineros.put(1)
-                        print(f'Un cocinero esta cocinando')
-                        time.sleep(4)
-                        ordenes.get()
-                        print(f'La orden esta lista')
-                        cocineros.get()
-                        ordenes_finalizadas.put(1)
-                        self.monitor.notify()
-                    else:
-                        print(f'Los cocineros estan ocupados')
-                        time.sleep(4)
+            with mutex:
+                if ordenes.empty() == False:
+                    print("Cocinero esta cocinando...")
+                    ordenes.get()
+                    time.sleep(2)
+                    print("Cocinero termino de cocinar una orden")
+                    ordenes_listas.put(1)
+                    time.sleep(3)
+                else:
+                    print("Cocinero esta esperando ordenes...")
+                    time.sleep(3)
 
     def run(self):
         self.cocinar()
 
-class Recepcionista(threading.Thread):
-    def __init__(self, id):
+
+class Mesero(threading.Thread):
+    def __init__(self):
         threading.Thread.__init__(self)
         self.monitor = threading.Condition()
-        self.id = id
 
-    def atender(self):
+    def servir(self):
         while True:
-            with self.monitor:
-                if reservaciones.qsize() > 2:
-                    capacidad.put(self.id)
-                    print(f'Cliente {self.id} con una reservacion fue atendido')
-                    reservaciones.get()
-                    self.monitor.notify()
-                    time.sleep(5)
+            with mutex:
+                if clientes.empty() == False:
+
+                    print("Mesero esta atendiendo...")
+                    time.sleep(2)
+                    print("Mesero termino de atender a un cliente y genero una orden")
+                    ordenes.put(1)
+                    time.sleep(2)
                 else:
-                    if clientes_pause.empty():
-                        if not capacidad.full():
-                            capacidad.put(self.id)
-                            print(f'Cliente {self.id} fue atendido exitosamente')
-                            self.monitor.notify()
-                            time.sleep(4)
-                        else:
-                            print(f'Cliente {self.id} esta en cola de espera')
-                            clientes_pause.put(self.id)
-                            print(f'Hay {clientes_pause.qsize()} clientes en cola de espera')
-                            time.sleep(8)
-                    else:
-                        print(f'Cliente {self.id} esta en cola de espera')
-                        clientes_pause.put(self.id)
-                        print(f'Hay {clientes_pause.qsize()} clientes en cola de espera')
-                        time.sleep(5)
-            self.id = self.id + 1
+                    print("Mesero esta esperando clientes...")
+                    time.sleep(2)
 
     def run(self):
-        self.atender()
+        self.servir()
 
-if __name__ == '__main__':
-    hilo_recepcion = Recepcionista(1)
-    hilo_recepcion.start()
-    hilo_cliente = Cliente(1)
-    hilo_cliente.start()
-    hilo_reservacion = Reservacion(1)
-    hilo_reservacion.start()
-    hilo_mesero = Mesero()
-    hilo_mesero.start()
-    hilo_cocinero = Cocinero()
-    hilo_cocinero.start()
+
+if __name__ == "__main__":
+
+    atender = Atender(1)
+    atender.start()
+
+    cliente = Cliente()
+    cliente.start()
+
+    reservacion = Reservacion(1)
+    reservacion.start()
+
+    for i in range(MESEROS):
+        mesero = Mesero()
+        mesero.start()
+
+    for i in range(COCINEROS):
+        cocinero = Cocinero()
+        cocinero.start()
